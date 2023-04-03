e4cede5fccac900a4e205a1b6e501b67a523c28a
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-04-03 21:18:23 +0300

    linked lists

--- Data Structures/LinkedLists.cpp
+++ Data Structures/LinkedLists.cpp
@@ -6,3 +6,32 @@ struct Node
     int data;
     Node *next;
 };
+
+int main()
+{
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "";
+        temp = temp->next;
+    }
+
+    return 0;
+}
