33be46b715e508cfdf48e39110e783a74581caaf
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-04-08 16:49:57 +0300

    node has a parent

--- Data Structures/Trees.cpp
+++ Data Structures/Trees.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
+struct Node
+{
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 };
+
+int main()
+{
+    Node *root = new Node();
+    Node *left = new Node();
+    Node *right = new Node();
+
+    root->data = 1;
+    root->left = left;
+    root->right = right;
+
+    left->data = 2;
+    left->left = NULL;
+    left->right = NULL;
+
+    right->data = 3;
+    right->left = NULL;
+    right->right = NULL;
+
+    cout << root->data << "" << root->left->data << "" << root->right->data;
+    return 0;
+}
